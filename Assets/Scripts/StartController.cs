5ace0f4117d2962e513761d02cbfbb79b9a7fccc
宮下 <miyashita0507@gmail.com>
2022-01-29 11:48:50 +0900

    新規プレハブ、Script追加

---
+++ Assets/Scripts/StartController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug用(ゲーム開始時にSetupは呼ばれる)
+        PlayerInfoManager.instance.SetUp();
+    }
+
+
+}
