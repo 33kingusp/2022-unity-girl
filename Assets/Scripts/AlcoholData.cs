3c68e2ae1948302428e93b3b78407d2e5c50085e
宮下 <miyashita0507@gmail.com>
2022-01-29 18:43:18 +0900

    ボタン追加

--- Assets/Scripts/AlcoholData.cs
+++ Assets/Scripts/AlcoholData.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class AlcoholData
 {
-    public static readonly string[] AlcoholName = { "ビール", "ワイン", "日本酒", "カクテル" };
-    public static readonly int[] AlcoholDegree = { 5,12,15,20};
-    public static readonly int[] AlcoholPrice = { 500,600,800,800};
+    public static readonly string[] AlcoholName = { "水","ビール", "ワイン", "日本酒", "カクテル","退出"};
+    public static readonly int[] AlcoholDegree = {0, 5,12,15,20,0};
+    public static readonly int[] AlcoholPrice = {0,500,600,800,800,0};
 
     public static readonly string[] SpritePath = {
+        "btn_water.png",
         "btn_beer.png",
         "btn_wine.png",
         "btn_sake.png",
         "btn_cocktail.png",
+        "btn_exit.png",
     };
 }
