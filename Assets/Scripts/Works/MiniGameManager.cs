11eb5f9543d01f5085bdb15314355fd1d38c6972
SeiyaTougashi <y.lan490@gmail.com>
2022-01-30 11:08:48 +0900

    commit05
    バグ修正
    紙生成
    時間計測追加
    等

--- Assets/Scripts/Works/MiniGameManager.cs
+++ Assets/Scripts/Works/MiniGameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
+using Logics;
 
 namespace Works
 {
@@ -13,6 +14,10 @@ namespace Works
         private GameObject _paper;
         private GameObject _paper2;
         private GameObject clickedDownGameObject;   // クリックダウン時オブジェクト
+        private int[] _paperCnt;    // 紙のカウント
+        private float time; // 経過時間を格納する変数
+        private Text timerText;
+        private const float timeUp = 30.0f;
 
         // Start is called before the first frame update
         void Start()
@@ -23,26 +28,47 @@ namespace Works
             Text score_text = score_object.GetComponent<Text>();
             // テキストの表示を入れ替える
             score_text.text = "Score : 0";
-            _paper = Instantiate(_paperInfo[(int)Random.Range(0, _paperInfo.Length)]);
-            _paper.transform.position = new Vector3(7.0f, 1.5f, -1);
-            _paper2 = Instantiate(_paperInfo[(int)Random.Range(0, _paperInfo.Length)]);
-            _paper2.transform.position = new Vector3(7.0f, -2.5f, -1);
+            // 紙配置
+            GameObject paper = Instantiate(_paperInfo[(int)Random.Range(0, _paperInfo.Length)]);
+            paper.transform.position = new Vector3(7.0f, 1.5f, -1);
+            paper = Instantiate(_paperInfo[(int)Random.Range(0, _paperInfo.Length)]);
+            paper.transform.position = new Vector3(7.0f, -2.5f, -1);
+            // 時間計測
+            time = 0.0f;
+            GameObject time_object = GameObject.Find("Time");
+            timerText = time_object.GetComponent<Text>();
+            timerText.text = "Time : 0";
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            // 時間計測
+            time += Time.deltaTime;
+            float text_time = Mathf.Max(0, timeUp - time);
+            timerText.text = "Time : " + text_time.ToString("0");
+            // 時間切れ→シーン遷移
+            if(timerText < 0)
+            {
+                GameLogicManager.instance.NextPhase();
+            }
         }
 
         public void changeScore(int val)
         {
             score += val;
+            score = Mathf.Max(0, score);
             // オブジェクトからTextコンポーネントを取得
             GameObject score_object = GameObject.Find("Score");
             Text score_text = score_object.GetComponent<Text>();
             // テキストの表示を入れ替える
             score_text.text = "Score : " + score;
+        }
+
+        public void createPaper(Vector3 pos)
+        {
+            GameObject paper = Instantiate(_paperInfo[(int)Random.Range(0, _paperInfo.Length)]);
+            paper.transform.position = pos;
         }
     }
 }
